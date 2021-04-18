be962234761750eabc81b1c3fb5946f86812e3f0
Doug C <82660360+dougc89@users.noreply.github.com>
2021-04-18 07:45:55 -0400

    Update who_heals_the_sick.js

--- who_heals_the_sick.js
+++ who_heals_the_sick.js
@@ -4,16 +4,30 @@ util.setQuestion('Who heals the sick, you or God?');
 util.setAnswer(`
 Yes. 
 <br><br>
-There are those of you who are asking the wrong question; seeking to trap a man in his words by claiming that man can do what only God can do. 
-To you, I just say "Yes, the sick are healed."
+There are those of you who are asking the wrong question, seeking to trap a man in his words by claiming that man can do what only God can do. 
+To you, I just say, "Yes, the sick are healed."
 <br><br>
 To those of you who are asking the right question, wondering what it looks like for us to operate in partnership with God to make the impossible possible, 
-I say "You give them something to eat."
+I say, "You give them something to eat."
 <br><br>
-
-
-
+Jesus said this to His disciples when they fed a large multitude of people with absurdly too little food as a starting point. 
+It is preposterous to propose that miraculous healings are sourced from human resources. Human resources have their capabilities in medicine, which is a wonderful part of healing the sick,
+using the tools of man to save and restore lives. But to simply touch a person, or speak a few words, and have that person's health restored clearly is not the result of human resources.
+It would be rediculous to propose that those 5000+ people were fed by those 5 loaves and 2 fish.
+<br><br>
+However, Jesus did indeed instruct His disciples to feed those people. He gave them some simple instructions, and then He handed them the food to distribute. The people needed food, 
+for they were hungry and had no reasonable way of solving that problem on their own. The disciples were holding the food for them to eat, because Jesus gave it to them.
+Healing the sick is like this. 
+<br><br>
+In Acts, there is recorded an instance where Peter and John are on their way to the temple, and Peter says to a man there begging, who cannot walk, "I do not possess silver or gold,
+but <strong>what I do have I give to you</strong>: In the name of Jesus Christ the Nazarene -- walk!" And the man gets up jumping and walking, restored to health.
+<br><br>
+...
+<br><br>
+When a person needs healing in their body, God has given us what they need; namely the faith in the name of Jesus to make them well. 
+This faith is itself a gift from God, not something we worked up in ourselves. And the Lord has told us to give them what they need, not to send them away hungry.
 `);
+
 util.addScripture('Luke 9:11-17',
 `[The crowds followed Him] and welcoming them, He began speaking to them about the kingdom of God and curing those who had need of healing.
 <br><br>
@@ -26,3 +40,5 @@ And He said to His disciples, <span class='jesus-red'>"Have them sit down to eat
 They did so, and had them all sit down. Then He took the five loaves and the two fish, and looking up to heaven, He blessed them, and broke them, and kept giving them to the disciples 
 to set before the people. And they all ate and were satisfied; and the broken pieces which they had left over were picked up, twelve baskets full.
 `);
+
+util.addScripture('Acts 3:6,12,16',``);
