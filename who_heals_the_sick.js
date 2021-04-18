3c5dc0853a75fafb18c187c6707d08df36ac3da4
Doug C <82660360+dougc89@users.noreply.github.com>
2021-04-18 00:33:11 -0400

    Create who_heals_the_sick.js
    start answer with Luke 9, feeding 5000 example

---
+++ who_heals_the_sick.js
@@ -0,0 +1,28 @@
+util.newTopic();
+util.setType(true, false); // body, blood
+util.setQuestion('Who heals the sick, you or God?');
+util.setAnswer(`
+Yes. 
+<br><br>
+There are those of you who are asking the wrong question; seeking to trap a man in his words by claiming that man can do what only God can do. 
+To you, I just say "Yes, the sick are healed."
+<br><br>
+To those of you who are asking the right question, wondering what it looks like for us to operate in partnership with God to make the impossible possible, 
+I say "You give them something to eat."
+<br><br>
+
+
+
+`);
+util.addScripture('Luke 9:11-17',
+`[The crowds followed Him] and welcoming them, He began speaking to them about the kingdom of God and curing those who had need of healing.
+<br><br>
+Now the day was ending, and the twelve came and said to Him, "Send the crowd away, that they may go into the surrounding villages and find lodging and get something to eat; 
+for here we are in a desolate place." But He said to them, <span class='jesus-red'>"You give them something to eat!"</span>
+<br><br>
+And they said, "We have no more than five loaves and two fish, unless we go and buy food for all these people." (For there were about five thousand men.) 
+And He said to His disciples, <span class='jesus-red'>"Have them sit down to eat in groups of about fifty each."</span>
+<br><br>
+They did so, and had them all sit down. Then He took the five loaves and the two fish, and looking up to heaven, He blessed them, and broke them, and kept giving them to the disciples 
+to set before the people. And they all ate and were satisfied; and the broken pieces which they had left over were picked up, twelve baskets full.
+`);
