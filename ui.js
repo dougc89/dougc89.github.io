0d19d4ef03a602163abf9f78ba664e3ec442573d
dougc89 <dougc89@vt.edu>
2024-06-11 13:13:14 -0400

    push ui

--- ui.js
+++ ui.js
@@ -1,13 +1,12 @@
 const { createApp } = Vue
 const { createVuetify } = Vuetify
 
+const vuetify3 = createVuetify()
 const app = createApp({
-    el: '#app',
-    vuetify: createVuetify(),
     data: function(){
         return {
             message: "Hello to the World"
         }
     }
 })
-// app.use(vuetify3).mount('#app')
+app.use(vuetify3).mount('#app')
