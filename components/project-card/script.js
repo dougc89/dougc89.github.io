6ff3cda28b8fd7b0ad4d9364e263db3430314b46
dougc89 <dougc89@vt.edu>
2024-06-11 13:49:56 -0400

    init a component

---
+++ components/project-card/script.js
@@ -0,0 +1,21 @@
+export default Vue.component('project-card',{
+    template: '#project-card-template',
+    props:{
+        project: {
+            default: null
+        }
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed: {
+        title(){
+            return this.project ? `${this.project.title}` :  'Title goes here...'
+        },
+        subtitle(){
+            return this.project ? `${this.project.date}` :  'Month of completion goes here...'    
+        }
+    }
+})
--- ui.js
+++ ui.js
@@ -1,6 +1,8 @@
 const { createApp } = Vue
 const { createVuetify } = Vuetify
 
+import project_card from 'components/project-card/script.js'
+
 const vuetify3 = createVuetify()
 const app = createApp({
     data: function(){
