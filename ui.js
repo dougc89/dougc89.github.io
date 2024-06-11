0d93621df754e60de7209fbb5350a207975132f5
dougc89 <dougc89@vt.edu>
2024-06-11 14:19:33 -0400

    include here

--- ui.js
+++ ui.js
@@ -1,12 +1,33 @@
 const { createApp } = Vue
 const { createVuetify } = Vuetify
 
-import project_card from '../components/project-card/script.js'
+//import ProjectCard from '../components/project-card/script.js'
+const ProjectCard = {
+    template: '#project-card-template',
+    props:{
+        project: {
+            default: null
+        }
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed: {
+        title(){
+            return this.project ? `${this.project.title}` :  'Title goes here...'
+        },
+        subtitle(){
+            return this.project ? `${this.project.date}` :  'Month of completion goes here...'    
+        }
+    }
+}
 
 const vuetify3 = createVuetify()
 const app = createApp({
     components: {
-        project_card
+        ProjectCard
     },
     data: function(){
         return {
